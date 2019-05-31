600c3425f42330373f3d87fe201f9cf3b0e05e8d
Jorge Hevia <jmhev@outlook.com>
2019-05-31 16:38:06 +0200

    OpenIntervalTest: added inverse test cases for endpoints.

--- intervalHierarchy/src/test/java/tdd/intervalHierarchy/OpenIntervalTest.java
+++ intervalHierarchy/src/test/java/tdd/intervalHierarchy/OpenIntervalTest.java
@@ -20,4 +20,18 @@ public class OpenIntervalTest {
 		Interval another = new IntervalBuilder().min(-1).max(3).closed().build();
 		assertFalse(one.isIntersected(another));
 	}
+	
+	@Test
+	public void testIsNotIntersectedWhenMinEqualsMaxBothOpen() {
+		Interval one = new IntervalBuilder().min(3).max(14).build();
+		Interval another = new IntervalBuilder().min(14).max(16).build();
+		assertFalse(one.isIntersected(another));
+	}
+	
+	@Test
+	public void testIsNotIntersectedWhenMinEqualsMaxOneOpenOneClosed() {
+		Interval one = new IntervalBuilder().min(3).max(14).build();
+		Interval another = new IntervalBuilder().min(14).max(16).closed().build();
+		assertFalse(one.isIntersected(another));
+	}
 }
