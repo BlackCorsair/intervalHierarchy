1fa7f254b794d90ffd8c878e5d833e83407b779e
Jorge Hevia <jmhev@outlook.com>
2019-06-02 20:11:28 +0200

    Refactoring. Refactored old test cases into new design TC.

--- intervalHierarchy/src/test/java/tdd/intervalHierarchy/IntervalTest.java
+++ intervalHierarchy/src/test/java/tdd/intervalHierarchy/IntervalTest.java
@@ -1,6 +1,7 @@
 package tdd.intervalHierarchy;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -21,5 +22,40 @@ public class IntervalTest {
 		Interval one = new IntervalBuilder().min(new EndPoint(3, false)).max(new EndPoint(14, false)).build();
 		Interval another = new IntervalBuilder().min(new EndPoint(7, false)).max(new EndPoint(15, false)).build();
 		assertTrue(one.isIntersected(another));
+	}
+	
+	@Test
+	public void testIsIntersectedOverlapingByEquals() {
+		Interval one = new IntervalBuilder().min(new EndPoint(3, false)).max(new EndPoint(14, false)).build();
+		Interval another = new IntervalBuilder().min(new EndPoint(3, false)).max(new EndPoint(14, false)).build();
+		assertTrue(one.isIntersected(another));
+	}
+	
+	@Test
+	public void testIsIntersectedOverlapingByBoth() {
+		Interval one = new IntervalBuilder().min(new EndPoint(3, false)).max(new EndPoint(14, false)).build();
+		Interval another = new IntervalBuilder().min(new EndPoint(0, false)).max(new EndPoint(17, false)).build();
+		assertTrue(one.isIntersected(another));
+	}
+	
+	@Test
+	public void testIsIntersectedOverlapingByInside() {
+		Interval one = new IntervalBuilder().min(new EndPoint(3, false)).max(new EndPoint(14, false)).build();
+		Interval another = new IntervalBuilder().min(new EndPoint(5, false)).max(new EndPoint(10, false)).build();
+		assertTrue(one.isIntersected(another));
+	}
+	
+	@Test
+	public void testIsIntersectedNotOverlapingByLeft() {
+		Interval one = new IntervalBuilder().min(new EndPoint(3, false)).max(new EndPoint(14, false)).build();
+		Interval another = new IntervalBuilder().min(new EndPoint(0, false)).max(new EndPoint(2, false)).build();
+		assertFalse(one.isIntersected(another));
+	}
+	
+	@Test
+	public void testIsIntersectedNotOverlapingByRight() {
+		Interval one = new IntervalBuilder().min(new EndPoint(3, false)).max(new EndPoint(14, false)).build();
+		Interval another = new IntervalBuilder().min(new EndPoint(16, false)).max(new EndPoint(22, false)).build();
+		assertFalse(one.isIntersected(another));
 	}
 }
