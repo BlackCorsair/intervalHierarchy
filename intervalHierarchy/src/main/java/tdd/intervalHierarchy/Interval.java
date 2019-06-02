d20e97a4350a285e8a4edf0b080764398aa3e8a1
Jorge Hevia <jmhev@outlook.com>
2019-06-02 20:50:29 +0200

    Refactoring Inverval. Readability changes.
    * Moved conditions to functions in other to make the code more legible.

--- intervalHierarchy/src/main/java/tdd/intervalHierarchy/Interval.java
+++ intervalHierarchy/src/main/java/tdd/intervalHierarchy/Interval.java
@@ -1,36 +1,42 @@
 package tdd.intervalHierarchy;
+
 /*
  * This is an Utopy non-tested
  */
 public class Interval {
 	private EndPoint min;
 	private EndPoint max;
-	
+
 	public Interval(EndPoint min, EndPoint max) {
 		this.min = min;
 		this.max = max;
 	}
-	
+
 	public boolean isIntersected(Interval another) {
 		return isIncluded(another.getMax()) || isIncluded(another.getMin())
-				|| min.getPositionFrom(another.getMin()) == Position.RIGHT
-					&& max.getPositionFrom(another.getMax()) == Position.LEFT
-				|| min.getPositionFrom(another.getMin()) == null
-					&& max.getPositionFrom(another.getMax()) == null;
+				|| intersectedWhenIncludedBy(another)
+				|| intersectedWithEqualAndOpenInterval(another);
 	}
 	
+	private boolean intersectedWhenIncludedBy(Interval another) {
+		return min.getPositionFrom(another.getMin()) == Position.RIGHT
+				&& max.getPositionFrom(another.getMax()) == Position.LEFT;
+	}
+
+	private boolean intersectedWithEqualAndOpenInterval(Interval another) {
+		return min.getPositionFrom(another.getMin()) == null && max.getPositionFrom(another.getMax()) == null;
+	}
+
 	public boolean isIncluded(EndPoint point) {
-		return min.getPositionFrom(point) == Position.LEFT
-				&& max.getPositionFrom(point) == Position.RIGHT
-				|| min.getPositionFrom(point) == Position.TOP
-				|| max.getPositionFrom(point) == Position.TOP;
-				
+		return min.getPositionFrom(point) == Position.LEFT && max.getPositionFrom(point) == Position.RIGHT
+				|| min.getPositionFrom(point) == Position.TOP || max.getPositionFrom(point) == Position.TOP;
+
 	}
-	
+
 	public EndPoint getMin() {
 		return this.min;
 	}
-	
+
 	public EndPoint getMax() {
 		return this.max;
 	}
